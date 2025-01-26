11e3aaff5f09376ad2ccc3687006b2d235ceca72
Er. Anil Kumar Thakur <anilkumarthakur60@gmail.com>
2025-01-26 20:35:03 +0545

    chore:

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,10 @@ import tseslint from 'typescript-eslint'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    ignores: ['node_modules/**', 'dist/**', 'package-lock.json']
+  },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended
